f2a1876aa67a71b63367037109db437d11d114e9
Gabriella Guimarães <gabigara13@gmail.com>
2026-05-05 20:11:04 -0300

    feat: adiciona modelagem de temporadas e busca completa da série
    - Cria record DadosTemporada com lista de episódios
    - Mapeia campos JSON com @JsonAlias (Season, Episodes)
    - Adiciona @JsonIgnoreProperties para ignorar campos não utilizados
    - Implementa loop para buscar todas as temporadas da série
    - Torna URL dinâmica com base no número da temporada
    - Armazena temporadas em lista
    - Exibe dados utilizando forEach e method reference

--- src/main/java/br/com/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package br.com.screenmatch;
 
 import br.com.screenmatch.model.DadosEpisodio;
 import br.com.screenmatch.model.DadosSerie;
+import br.com.screenmatch.model.DadosTemporada;
 import br.com.screenmatch.service.ConsumoApi;
 import br.com.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -22,8 +26,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 			throw new RuntimeException("API key não configurada");
 		}
 
-		String enderecoSerie = "http://www.omdbapi.com/?t=the+office&apikey=" + apiKey;
-		String enderecoEpisodio = "http://www.omdbapi.com/?t=the+office&season=1&episode=2&apikey=" + apiKey;
+		String enderecoSerie = "https://www.omdbapi.com/?t=the+office&apikey=" + apiKey;
+		String enderecoEpisodio = "https://www.omdbapi.com/?t=the+office&season=1&episode=2&apikey=" + apiKey;
 
 		var consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados(enderecoSerie);
@@ -36,5 +40,16 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		json = consumoApi.obterDados(enderecoEpisodio);
 		DadosEpisodio dadosEpisodio = conversor.obterdados(json, DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for(int i = 1; i <= dados.totalTemporadas(); i++) {
+			String enderecoTemporada = "https://www.omdbapi.com/?t=the+office&season=" + i + "&apikey=" + apiKey;
+			json = consumoApi.obterDados(enderecoTemporada);
+			DadosTemporada dadosTemporada = conversor.obterdados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
 	}
 }
