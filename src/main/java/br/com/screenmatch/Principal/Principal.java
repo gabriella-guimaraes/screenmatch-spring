63b6d6f67e30ba4d5096d85daa77686d28f8274d
Gabriella Guimarães <gabigara13@gmail.com>
2026-05-11 19:35:04 -0300

    feat: percorre coleções de temporadas e episódios utilizando lambdas

--- src/main/java/br/com/screenmatch/Principal/Principal.java
+++ src/main/java/br/com/screenmatch/Principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.screenmatch.Principal;
 
+import br.com.screenmatch.model.DadosEpisodio;
 import br.com.screenmatch.model.DadosSerie;
 import br.com.screenmatch.model.DadosTemporada;
 import br.com.screenmatch.service.ConsumoApi;
@@ -44,6 +45,15 @@ public class Principal {
 		}
 
 		temporadas.forEach(System.out::println);
+
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//
+//            for(int j = 0; j < episodiosTemporada.size(); j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo()))); // Lambda
 
     }
 }
