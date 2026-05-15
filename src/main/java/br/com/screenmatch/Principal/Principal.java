179f884ecc46f5c1ae2cfe2875de5d3f1de105f1
Gabriella Guimarães <gabigara13@gmail.com>
2026-05-15 14:22:00 -0300

    feat: implementa operações encadeadas com streams e lambdas (estudos)

--- src/main/java/br/com/screenmatch/Principal/Principal.java
+++ src/main/java/br/com/screenmatch/Principal/Principal.java
@@ -1,12 +1,12 @@
 package br.com.screenmatch.Principal;
 
-import br.com.screenmatch.model.DadosEpisodio;
 import br.com.screenmatch.model.DadosSerie;
 import br.com.screenmatch.model.DadosTemporada;
 import br.com.screenmatch.service.ConsumoApi;
 import br.com.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,42 +18,44 @@ public class Principal {
     private ConverteDados conversor = new ConverteDados();
 
     public void exibeMenu() {
-        System.out.println("Digite o nome da série para busca: ");
-
-        var nomeSerie = leitura.nextLine();
-
-        if (API_KEY == null) {
-            throw new RuntimeException("API key não configurada");
-        }
-
-        var enderecoSerie = ENDERECO + nomeSerie.replace(" ", "+") + API_KEY;
-
-        var json = consumoApi.obterDados(enderecoSerie);
-        System.out.println(json);
-
-        DadosSerie dados = conversor.obterdados(json, DadosSerie.class);
-        System.out.println(dados);
-
-
-        List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for(int i = 1; i <= dados.totalTemporadas(); i++) {
-			String enderecoTemporada = ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY;
-			json = consumoApi.obterDados(enderecoTemporada);
-			DadosTemporada dadosTemporada = conversor.obterdados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-
-		temporadas.forEach(System.out::println);
-
-//        for (int i = 0; i < dados.totalTemporadas(); i++) {
-//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//        System.out.println("Digite o nome da série para busca: ");
 //
-//            for(int j = 0; j < episodiosTemporada.size(); j++) {
-//                System.out.println(episodiosTemporada.get(j).titulo());
-//            }
+//        var nomeSerie = leitura.nextLine();
+//
+//        if (API_KEY == null) {
+//            throw new RuntimeException("API key não configurada");
 //        }
-        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo()))); // Lambda
+//
+//        var enderecoSerie = ENDERECO + nomeSerie.replace(" ", "+") + API_KEY;
+//
+//        var json = consumoApi.obterDados(enderecoSerie);
+//        System.out.println(json);
+//
+//        DadosSerie dados = conversor.obterdados(json, DadosSerie.class);
+//        System.out.println(dados);
+//
+//
+//        List<DadosTemporada> temporadas = new ArrayList<>();
+//
+//		for(int i = 1; i <= dados.totalTemporadas(); i++) {
+//			String enderecoTemporada = ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY;
+//			json = consumoApi.obterDados(enderecoTemporada);
+//			DadosTemporada dadosTemporada = conversor.obterdados(json, DadosTemporada.class);
+//			temporadas.add(dadosTemporada);
+//		}
+//
+//		temporadas.forEach(System.out::println);
+//
+//        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo()))); // Lambda
+
+        List<String> jedis = Arrays.asList("Anakin Skywalker", "Obi-Wan Kenobi", "Mace Windu", "Yoda");
+
+        jedis.stream() // operações encadeadas
+                .sorted()
+                .limit(2)
+                .filter(n -> n.startsWith("A"))
+                .map(n -> n.toUpperCase())
+                .forEach(System.out::println);
 
     }
 }
